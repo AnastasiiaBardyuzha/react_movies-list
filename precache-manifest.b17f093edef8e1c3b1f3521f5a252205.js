441dfc3c4e309d3574b2442e81ed519db0509e4b
Anastasiia Bardyuzha <stasya.meshcheryakova@gmail.com>
2020-02-13 00:00:37 +0200

    Updates

---
+++ precache-manifest.b17f093edef8e1c3b1f3521f5a252205.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "579d82b7ca049fb6f19de146a0aed89d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5a99663c094603f25f33",
+    "url": "./static/css/2.70d9d258.chunk.css"
+  },
+  {
+    "revision": "13b72cb1bf9a02c93750",
+    "url": "./static/css/main.08ff34a6.chunk.css"
+  },
+  {
+    "revision": "5a99663c094603f25f33",
+    "url": "./static/js/2.237f35df.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.237f35df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "13b72cb1bf9a02c93750",
+    "url": "./static/js/main.885c5774.chunk.js"
+  },
+  {
+    "revision": "ef79a84613c281fd2d2e",
+    "url": "./static/js/runtime-main.e88ca7d2.js"
+  }
+]);
